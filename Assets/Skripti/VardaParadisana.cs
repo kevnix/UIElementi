27eaacfe3c53c21b6ae9b3dca289abec381abcad
Volters <kevinsvolters12@gmail.com>
2023-03-29 09:18:08 +0300

    skripta saakshana

---
+++ Assets/Skripti/VardaParadisana.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VardaParadisana : MonoBehaviour {
+	//maingais kas ieglaba textu no inputfield
+	private string text;
+	private string[] options = { "Sveiks!", "Jauku dienu!", "Tinies!", "LOL", ":tf:" };
+	int x;
+
+	public GameObject inputfield;
+	public GameObject outputfield;
+
+	public void Uzglabat() 
+	{
+		x = Random.Range(0, options.Length);
+		text = inputfield.GetComponent<Text>().text;
+		outputfield.GetComponent<Text>().text = options[x]+text.ToUpper()+"!";
+	}
+	/*int kadrs = 0;
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Startēta programma!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		kadrs++;
+		Debug.Log("Kadrs: "+kadrs);
+	}*/
+}
