c261156c24a62f8a2ce8183308d88cd488dc3a9d
Volters <kevinsvolters12@gmail.com>
2023-03-31 09:19:07 +0300

    toggle images

---
+++ Assets/Skripti/ShowImage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowImage : MonoBehaviour {
+    public GameObject bean;
+    public GameObject lacis;
+    public GameObject tante;
+    public GameObject masina;
+
+    public void BeanImage(bool vertiba)
+    {
+        bean.SetActive(vertiba);
+    }
+
+    public void LacisImage(bool vertiba)
+    {
+        lacis.SetActive(vertiba);
+    }
+
+    public void TanteImage(bool vertiba)
+    {
+        tante.SetActive(vertiba);
+    }
+
+    public void MasinaImage(bool vertiba)
+    {
+        masina.SetActive(vertiba);
+    }
+}
